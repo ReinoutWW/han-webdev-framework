2f9127d428b9ecf9fc5e3afa3caf1ffa56ddfb0f
Reinout Wijnholds <ReinoutWijnholds2002@gmail.com>
2024-05-22 15:34:20 +0200

    Start with the framework

---
+++ applicatie/public/index.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+dd('Hello world!');
+
+// Request received
+
+// Perform some logic
+
+// Send response (string of content)
+
+echo 'Hello world';
+
+?>
